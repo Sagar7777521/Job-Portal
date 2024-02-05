0c217e9b9edefb7e709bcfc49b1dec38070cb3d5
Sagar <70952463+Sagar7777521@users.noreply.github.com>
2024-02-06 01:05:29 +0530

    Header-Navigation-Update

---
+++ app/(router)/_components/SideNav.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { BadgeIcon, BookOpen, GraduationCap } from "lucide-react";
+import Image from "next/image";
+
+const SideNav = () => {
+  const menu = [
+    {
+      id: 1,
+      name: "All courses",
+      icon: BookOpen,
+    },
+    {
+      id: 2,
+      name: "Membership",
+      icon: BadgeIcon,
+    },
+    {
+      id: 3,
+      name: "Be Instructor",
+      icon: GraduationCap,
+    },
+  ];
+  return (
+    <div className="p-5 bg-white shadow-sm border h-screen">
+      <Image src="/Job-Logo.png" alt="Logo" height={100} width={90} />
+      <hr className="mt-7"></hr>
+      <div className="mt-6">
+        {menu.map((item, index) => (
+          <div className=" group flex gap-3 mt-1 p-3 text-[20px] items-center text-gray-700 cursor-pointer hover:bg-primary hover:text-white rounded-md transition-all ease-in-out-150">
+            <item.icon className="group-hover:animate-bounce" />
+            <h2>{item.name}</h2>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SideNav;
---
+++ app/(router)/courses/page.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Courses = () => {
+  return (
+    <div className="grid grid-cols-3">
+      {/* Left Container */}
+      <div className="col-span-2">{/* Banner */}
+      Lefter
+      </div>
+      {/* Right Container */}
+      Right Section
+    </div>
+  );
+};
+
+export default Courses;
